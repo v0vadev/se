f842befdd56f1978c2a3ee58cf19ad8cb3c65ea4
v0vadev <vova.aksenov@list.ru>
2019-11-23 22:38:11 +0300

    Updates

---
+++ precache-manifest.5d01c245ff75f8d38676baeb8f63572a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed156a2ef93cfe9a2d26b3eb8921bf0",
+    "url": "/se/index.html"
+  },
+  {
+    "revision": "34923db0bca66dcd1a0e",
+    "url": "/se/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "99fbea1594ec41a03e49",
+    "url": "/se/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "34923db0bca66dcd1a0e",
+    "url": "/se/static/js/2.b3894d9b.chunk.js"
+  },
+  {
+    "revision": "99fbea1594ec41a03e49",
+    "url": "/se/static/js/main.fde82133.chunk.js"
+  },
+  {
+    "revision": "c0d47cf010ccaa40adf8",
+    "url": "/se/static/js/runtime-main.f5d983cd.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/se/static/media/persik.4e1ec840.png"
+  }
+]);
